726013ed669861ba33d4179b04c50bc12f7e6706
KatenkaKet <katenkeket13@gmai.com>
2023-12-12 01:06:21 +0500

    add calculator

---
+++ Model.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcul_2
+{
+    class Calculator
+    {
+        public string s;
+        public int i = 0; // индекс элемента
+        public double ProcE() //результат
+        {
+            s += '\0';
+            double x = ProcT(); 
+            while (s[i] == '+' || s[i] == '-')
+            {
+                char p = s[i];
+                i++; if (p == '+')
+                {
+                    x += ProcT();
+                }
+                else
+                {
+                    x -= ProcT();
+                }
+            }
+            return x;
+        }
+        public double ProcT()
+        {
+            double x = ProcM();
+            while (s[i] == '*' || s[i] == '/')
+            {
+                char p = s[i]; i++;
+                if (p == '*')
+                {
+                    x *= ProcM();
+                }
+                else
+                {
+                    x /= ProcM();
+                }
+            }
+            return x;
+        }
+        public double ProcM()
+        {
+            double x = 0; if (s[i] == '(')
+            {
+                i++;
+                x = ProcE(); if (s[i] != ')')
+                {
+                    Console.WriteLine("missing ')'");
+                }
+                i++;
+            }
+            else
+            {
+                if (s[i] == '-')
+                {
+                    i++;
+                    x -= ProcM();
+                }
+                else
+                {
+                    if (s[i] >= '0' && s[i] <= '9')
+                    {
+                        x = ProcC();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Syntex error.");
+                    }
+                }
+            }
+            return x;
+        }
+        public double ProcC()
+        {
+            double x = 0;
+            while (s[i] >= '0' && s[i] <= '9')
+            {
+                x *= 10; x += s[i] - '0';
+                i++;
+            }
+            double j = 0; if (s[i] == ',')
+            {
+                i++;
+                while (s[i] >= '0' && s[i] <= '9')
+                {
+                    x *= 10; x += s[i] - '0';
+                    i++; j++;
+                }
+                if (j != 0)
+                    x /= Math.Pow(10, j);
+            }
+            return x;
+        }
+        public int CheckSyntex(string s)
+        {
+            int bracket1 = 0;
+            int bracket2 = 0;
+            return 1;
+        }
+    }
+
+}
---
+++ ViewModel.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace Calcul_2
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private readonly Calculator _model;
+
+        private string _result;
+        private bool _flag = false;
+
+        public ICommand PushEnd{get; }
+        public ICommand DeleteLast { get; }
+        public ICommand Equal { get; }
+        public ICommand Delete {  get; }
+
+        public string Result
+        {
+            get { return _result; }
+            set { _result = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+            }
+        }
+        public ViewModel()
+        {
+            _model = new Calculator();
+            PushEnd = new RelayCommand(ExecuteAdd);
+            DeleteLast = new RelayCommand(ExecuteDeleteLast);
+            Equal = new RelayCommand(ExecuteEqual);
+            Delete = new RelayCommand(ExecuteDelete);
+        }
+
+        private void ExecuteAdd(object parameter)
+        {
+            string operation = "+-*/";
+            string temp = parameter as string;
+            if(this._flag == true)
+            {
+                if (operation.IndexOf(temp) >= 0)
+                {
+                    Result += temp;
+                }
+                else
+                {
+                    Result = "";
+                    Result += temp;
+
+                }
+                this._flag = false;
+            }
+            else
+            {
+                Result += temp;
+            }
+        }
+
+        private void ExecuteDeleteLast(object parameter)
+        {
+            if(Result != "")
+            {
+                Result = Result.Remove(Result.Length - 1);
+            }
+        }
+
+        private void ExecuteEqual(object parameter)
+        {
+            if(Result != "")
+            {
+                _model.s = Result;
+                _model.i = 0;
+                double temp = _model.ProcE();
+                Result = temp.ToString();
+                this._flag = true;
+            }
+        }
+
+        private void ExecuteDelete(object parameter)
+        {
+            Result = "";
+        }
+        
+
+
+    }
+
+
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+
+    
+
+
+
+
+
+}
